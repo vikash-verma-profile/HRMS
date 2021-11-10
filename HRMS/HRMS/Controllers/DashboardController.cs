b5f3c092c59c0a178465118784ab094cb3d7e81e
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-11-10 11:16:15 +0400

    first commit

--- HRMS/HRMS/Controllers/DashboardController.cs
+++ HRMS/HRMS/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRMS.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,16 @@ namespace HRMS.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
+        HRMSDBEntities db = new HRMSDBEntities();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetMenu()
+        {
+            var MenuData = db.tblMenuMasters.ToList();
+            return Json(MenuData, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ HRMS/HRMS/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRMS.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string MenuText { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public int IsParent { get; set; }
+    }
+}
