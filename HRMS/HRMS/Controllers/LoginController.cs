bbc8bdb92a94378e904683442a6d990bcf6c8f4a
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-11-10 08:08:34 +0400

    first commit

---
+++ HRMS/HRMS/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using HRMS.Entity;
+using HRMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRMS.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        HRMSDBEntities db = new HRMSDBEntities();
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Login login)
+        {
+            if(db.tblLogins.Any(x=>x.UserName==login.UserName && x.Password == login.Password))
+            {
+                //if condition is true come here
+            
+            
+            }
+            else
+            {
+                //if condition fails come here
+            }
+            return View();
+        }
+    }
+}
